8e5cd3f696e4f4ad6f44a6150ddd4931796bad6c
EdmundMartin <edmartin101@gmail.com>
2021-11-26 19:34:44 +0000

    Additional easy algo

---
+++ algoexpert/easy/remove_duplicates_from_ordered_linked_list.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from algoexpert.utils.linked_list import linked_list_to_python_list, linked_list_from_list, LinkedList
+
+
+def remove_duplicates_from_linked_list(head: LinkedList) -> LinkedList:
+    prev: Optional[LinkedList] = None
+    current_node = head
+    while current_node:
+        if prev and prev.value != current_node.value:
+            prev.next = current_node
+            prev = current_node
+        if prev and current_node.next is None:
+            if current_node.value == prev.value:
+                prev.next = None
+        if not prev:
+            prev = current_node
+        current_node = current_node.next
+    return head
+
+
+def remove_duplicates_from_linked_list_alternate(head: LinkedList) -> LinkedList:
+    current_node = head
+    while current_node:
+        next_node = current_node.next
+        while next_node is not None and next_node.value == current_node.value:
+            next_node = next_node.next
+
+        current_node.next = next_node
+        current_node = next_node
+    return head
+
+
+if __name__ == '__main__':
+    test_array = [1, 2, 3, 4, 4, 5, 5, 6, 6, 8, 9]
+    expected_array = [1, 2, 3, 4, 5, 6, 8, 9]
+    linked_list = linked_list_from_list(test_array)
+    result = remove_duplicates_from_linked_list(linked_list)
+    assert expected_array == linked_list_to_python_list(result)
+
+    linked_list = linked_list_from_list(test_array)
+    result = remove_duplicates_from_linked_list_alternate(linked_list)
+    assert expected_array == linked_list_to_python_list(result)
---
+++ algoexpert/utils/linked_list.py
@@ -0,0 +1,31 @@
+from typing import Any, List, Optional
+
+
+class LinkedList:
+
+    def __init__(self, value: int):
+        self.value = value
+        self.next: Optional['LinkedList'] = None
+
+
+def linked_list_to_python_list(head) -> List[Any]:
+    values = []
+    while head:
+        values.append(head.value)
+        head = head.next
+    return values
+
+
+def linked_list_from_list(values: List[Any]):
+    dummy_head = LinkedList(0)
+    root = dummy_head
+    for val in values:
+        root.next = LinkedList(val)
+        root = root.next
+    return dummy_head.next
+
+
+if __name__ == '__main__':
+    test_array = [1, 2, 3, 4, 4, 5, 5, 6, 6, 8, 9]
+    result = linked_list_from_list(test_array)
+    print(linked_list_to_python_list(result))
