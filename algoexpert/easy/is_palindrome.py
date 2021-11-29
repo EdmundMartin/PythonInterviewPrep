adc087135608c9c905cd9aff4b2e1456d0d5a85d
EdmundMartin <edmartin101@gmail.com>
2021-11-29 21:50:47 +0000

    Another easy one

---
+++ algoexpert/easy/is_palindrome.py
@@ -0,0 +1,15 @@
+
+
+def is_palindrome(target: str) -> bool:
+    back_idx = len(target) - 1
+    for forward_idx, _ in enumerate(target):
+        if target[forward_idx] != target[back_idx]:
+            return False
+        if forward_idx >= back_idx:
+            return True
+        back_idx -= 1
+
+
+if __name__ == '__main__':
+    assert is_palindrome("tat") is True
+    assert is_palindrome("bobobs") is False
