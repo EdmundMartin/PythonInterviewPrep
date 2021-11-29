dfabb2fe3da48cb0b518cb4da406aa07366d992c
EdmundMartin <edmartin101@gmail.com>
2021-11-29 21:08:19 +0000

    One more algoexpert

---
+++ algoexpert/easy/generate_document.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+
+def generate_document(characters: str, document: str) -> bool:
+    char_map = defaultdict(int)
+    for char in characters:
+        char_map[char] += 1
+    for char in document:
+        count = char_map[char]
+        if count == 0:
+            return False
+        char_map[char] -= 1
+    return True
+
+
+if __name__ == '__main__':
+    test_document = "Edmund Martin"
+    test_chars_true = "dmundE artinM"
+    test_chars_false = "dmunde artin"
+    assert generate_document(test_chars_true, test_document) is True
+    assert generate_document(test_chars_false, test_document) is False
