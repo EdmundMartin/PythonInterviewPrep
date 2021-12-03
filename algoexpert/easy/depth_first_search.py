2f2c18929a636ac20bbe6ba36b5c2b9b43b8db45
EdmundMartin <edmartin101@gmail.com>
2021-12-03 20:25:37 +0000

    Another easy

---
+++ algoexpert/easy/depth_first_search.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Node:
+
+    def __init__(self, name: str):
+        self.children: List['Node'] = []
+        self.name = name
+
+    def add_child(self, name) -> 'Node':
+        self.children.append(Node(name))
+        return self
+
+    def depth_first_search(self, array: List[str]) -> List[str]:
+        array.append(self.name)
+        for child in self.children:
+            child.depth_first_search(array)
+        return array
