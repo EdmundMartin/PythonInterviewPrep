42c5ee40751eea8661b8796d443507484787cbb1
EdmundMartin <edmartin101@gmail.com>
2021-12-05 19:34:58 +0000

    More grinding

---
+++ algoexpert/medium/suffix_trie_construction.py
@@ -0,0 +1,29 @@
+
+
+class SuffixTrie:
+
+    def __init__(self, string: str):
+        self.root = {}
+        self.end_symbol = "*"
+        self.populate_suffix_trie(string)
+
+    def populate_suffix_trie(self, string: str):
+        for i in range(len(string)):
+            self.insert_substring_at(i, string)
+
+    def insert_substring_at(self, i: int, string: str):
+        node = self.root
+        for j in range(i, len(string)):
+            letter = string[j]
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+        node[self.end_symbol] = True
+
+    def contains(self, item: str):
+        node = self.root
+        for letter in item:
+            if letter not in node:
+                return False
+            node = node[letter]
+        return self.end_symbol in node
---
+++ algoexpert/medium/validate_bst.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from algoexpert.utils.binary_tree import BinaryTree
+
+
+def validation_helper(tree: BinaryTree, min_value: Union[float, int], max_value: Union[float, int]) -> bool:
+    if tree is None:
+        return True
+    if tree.value < min_value or tree.value >= max_value:
+        return False
+    left_valid = validation_helper(tree.left, min_value, tree.value)
+    right_valid = validation_helper(tree.right, tree.value, max_value)
+    return left_valid and right_valid
+
+
+def validate_bst(tree: BinaryTree) -> bool:
+    return validation_helper(tree, float('-inf'), float('inf'))
+
+
+if __name__ == '__main__':
+    valid_tree = BinaryTree(10)
+    valid_tree.left = BinaryTree(7)
+    valid_tree.left.left = BinaryTree(6)
+    valid_tree.left.right = BinaryTree(7)
+    assert validate_bst(valid_tree) is True
+
+    invalid_tree = BinaryTree(10)
+    invalid_tree.right = BinaryTree(12)
+    invalid_tree.right.left = BinaryTree(11)
+    invalid_tree.right.right = BinaryTree(8)
+    assert validate_bst(invalid_tree) is False
