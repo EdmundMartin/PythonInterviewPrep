a3587fc66dce6f3d4617c1f367e6f9f538f7dbec
EdmundMartin <edmartin101@gmail.com>
2021-12-19 19:17:05 +0000

    One more

---
+++ algoexpert/medium/breadth_first_search.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class GraphNode:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.children: List['GraphNode'] = []
+
+    def add_child(self, name: str) -> 'GraphNode':
+        self.children.append(GraphNode(name))
+        return self
+
+    def breadth_first_search(self, array: List[str]):
+        queue = [self]
+        while queue:
+            current = queue.pop(0)
+            array.append(current.name)
+            for child in current.children:
+                queue.append(child)
+        return array
+
+
