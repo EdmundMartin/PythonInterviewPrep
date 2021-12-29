58512c1c226edd47c0d3dd78faff82491e42d5f5
EdmundMartin <edmartin101@gmail.com>
2021-12-29 19:04:12 +0000

    Some easy for fun

---
+++ leetcode/1119_remove_vowels_from_string.py
@@ -0,0 +1,11 @@
+
+
+class Solution:
+    def removeVowels(self, s: str) -> str:
+        vowels = {"a", "e", "i", "o", "u"}
+        return "".join([s for s in s if s not in vowels])
+
+
+if __name__ == '__main__':
+    result = Solution().removeVowels("leetcodeisacommunityforcoders")
+    assert result == "ltcdscmmntyfrcdrs"
---
+++ leetcode/771_jewels_stones.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        all_jewels = set(jewels)
+        count = 0
+        for stone in stones:
+            if stone in all_jewels:
+                count += 1
+        return count
