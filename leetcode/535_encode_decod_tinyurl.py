5a2dae4b91965635c42809613e54b21f3839a009
EdmundMartin <edmartin101@gmail.com>
2021-12-30 19:56:30 +0000

    Yet Another Leetcode

---
+++ leetcode/535_encode_decod_tinyurl.py
@@ -0,0 +1,38 @@
+
+
+class Codec:
+
+    def __init__(self):
+        self.buckets = [None] * 10_000
+        self.length = 10_000
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        url_hash = abs(hash(longUrl))
+        url_bucket = url_hash % self.length
+        if self.buckets[url_bucket] is None:
+            self.buckets[url_bucket] = [(url_hash, longUrl)]
+        else:
+            self.buckets[url_bucket].append((url_hash, longUrl))
+        return str(url_hash)
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        bucket = int(shortUrl) % self.length
+        found_bucket = self.buckets[bucket]
+        if len(found_bucket) == 1:
+            return found_bucket[0][1]
+        for b in found_bucket:
+            obj_hash, url = b
+            if obj_hash == shortUrl:
+                return url
+
+
+if __name__ == '__main__':
+    codec = Codec()
+    res = codec.encode("https://edmundmartin.com")
+    print(res)
+    res = codec.decode(res)
+    print(res)
