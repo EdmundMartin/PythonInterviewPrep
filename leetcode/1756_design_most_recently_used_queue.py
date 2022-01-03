5a9e74547362605e6a46d1bf661e0b73e7abb37e
EdmundMartin <edmartin101@gmail.com>
2022-01-03 22:15:19 +0000

    One more leetcode

---
+++ leetcode/1756_design_most_recently_used_queue.py
@@ -0,0 +1,15 @@
+
+class MRUQueue:
+
+    def __init__(self, n: int):
+        self.values = [i for i in range(1, n + 1)]
+
+    def fetch(self, k: int) -> int:
+        real_idx = k - 1
+        value = self.values.pop(real_idx)
+        self.values.append(value)
+        return value
+
+
+if __name__ == '__main__':
+    queue = MRUQueue(8)
